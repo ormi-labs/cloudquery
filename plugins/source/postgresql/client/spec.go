c5e7d85025f7869bd9c9eba16f3e7a0214940225
AnPiakhota <andrei.pehota@geomotiv.com>
2023-09-26 18:44:10 +0200

    Implement syncBlock method (in progress)

--- plugins/source/postgresql/client/spec.go
+++ plugins/source/postgresql/client/spec.go
@@ -1,14 +1,35 @@
 package client
 
+type EntityName string
+
+const (
+	EntBlock       EntityName = "block"
+	EntTransaction            = "transaction"
+	EntLog                    = "log"
+	EntTrace                  = "trace"
+)
+
+type Entity struct {
+	Name    EntityName `json:"name,omitempty"`
+	Table   string     `json:"table,omitempty"`
+	Enabled bool       `json:"enable,omitempty"`
+}
+
 type Spec struct {
 	ConnectionString string   `json:"connection_string,omitempty"`
 	PgxLogLevel      LogLevel `json:"pgx_log_level,omitempty"`
 	CDCId            string   `json:"cdc_id,omitempty"`
 	RowsPerRecord    int      `json:"rows_per_record,omitempty"`
+	SyncMode         string   `json:"sync_mode,omitempty"`
+	Limit            int      `json:"limit,omitempty"`
+	Entities         []Entity `json:"entities,omitempty"`
 }
 
 func (s *Spec) SetDefaults() {
 	if s.RowsPerRecord < 1 {
 		s.RowsPerRecord = 1
+	}
+	if s.SyncMode == "" {
+		s.SyncMode = "table"
 	}
 }
--- plugins/source/postgresql/client/sync.go
+++ plugins/source/postgresql/client/sync.go
@@ -17,6 +17,14 @@ import (
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
+type entity struct {
+	tableName    string
+	arrowSchema  *arrow.Schema
+	recBuilder   *array.RecordBuilder
+	transformers []transformer
+	colNames     []string
+}
+
 func (c *Client) Sync(ctx context.Context, options plugin.SyncOptions, res chan<- message.SyncMessage) error {
 	if c.options.NoConnection {
 		return fmt.Errorf("no connection")
@@ -95,14 +103,167 @@ func (c *Client) syncTables(ctx context.Context, snapshotName string, filteredTa
 		}
 	}
 
-	for _, table := range filteredTables {
-		if err := c.syncTable(ctx, tx, table, res); err != nil {
+	c.logger.Info().Any("entities", c.pluginSpec.Entities).Msg("enabled entities")
+
+	names := make([]string, len(filteredTables))
+	for i, table := range filteredTables {
+		names[i] = table.Name
+	}
+	c.logger.Info().Strs("names", names).Msg("filtered tables")
+
+	switch c.pluginSpec.SyncMode {
+	case "table":
+		for _, table := range filteredTables {
+			if err := c.syncTable(ctx, tx, table, res); err != nil {
+				return err
+			}
+		}
+	case "block":
+		// syncBlock is altered method to process blockchain data in orderly fashion
+		if err := c.syncBlock(ctx, tx, filteredTables, res); err != nil {
 			return err
 		}
 	}
+
 	if err := tx.Commit(ctx); err != nil {
 		return fmt.Errorf("failed to commit sync transaction: %w", err)
 	}
+	return nil
+}
+
+func (c *Client) query(ctx context.Context, entity *entity, sql string, args ...any) error {
+	rows, err := c.Conn.Query(ctx, sql, args...)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		values, err := rows.Values()
+		if err != nil {
+			return err
+		}
+
+		for i, value := range values {
+			val, err := entity.transformers[i](value)
+			if err != nil {
+				return err
+			}
+
+			s := scalar.NewScalar(entity.arrowSchema.Field(i).Type)
+			if err := s.Set(val); err != nil {
+				return err
+			}
+
+			scalar.AppendToBuilder(entity.recBuilder.Field(i), s)
+		}
+	}
+	return nil
+}
+
+func (c *Client) syncBlock(ctx context.Context, tx pgx.Tx, tables []*schema.Table, res chan<- message.SyncMessage) error {
+	entities := make(map[EntityName]*entity)
+	valid := false
+LOOP:
+	for _, e := range c.pluginSpec.Entities {
+		if e.Enabled {
+			for _, table := range tables {
+				if table.Name == e.Table {
+					// there has to be block entity
+					if e.Name == EntBlock {
+						valid = true
+					}
+
+					arrowSchema := table.ToArrowSchema()
+					entities[e.Name] = &entity{
+						tableName:    table.Name,
+						arrowSchema:  arrowSchema,
+						recBuilder:   array.NewRecordBuilder(memory.DefaultAllocator, arrowSchema),
+						transformers: transformersForSchema(arrowSchema),
+						colNames:     make([]string, len(table.Columns)),
+					}
+					for i, col := range table.Columns {
+						entities[e.Name].colNames[i] = pgx.Identifier{col.Name}.Sanitize()
+					}
+					continue LOOP
+				}
+			}
+			// no table
+			return fmt.Errorf("no/wrong table specified for %q entity", e.Name)
+		} else {
+			entities[e.Name] = nil
+		}
+	}
+
+	if !valid {
+		return fmt.Errorf("no block entity specified in %q sync mode", "block")
+	}
+
+	// block
+	sql1 := `SELECT $1 FROM blocks ORDER BY $2 ASC LIMIT 10` // TODO
+	args1 := []any{
+		strings.Join(entities[EntBlock].colNames, ","),
+		//pgx.Identifier{entities[EntBlock].tableName}.Sanitize(),
+		pgx.Identifier{"number"}.Sanitize(),
+		//"number",
+	}
+	// transaction
+	sql2 := `SELECT $1 FROM transactions WHERE $3 = $4`
+	args2 := []any{
+		strings.Join(entities[EntTransaction].colNames, ","),
+		//pgx.Identifier{entities[EntTransaction].tableName}.Sanitize(),
+		pgx.Identifier{"block_hash"}.Sanitize(),
+		//"block_hash",
+	}
+
+	blocks, err := tx.Query(ctx, sql1, args1...)
+	if err != nil {
+		return err
+	}
+	defer blocks.Close()
+
+	var hashPos int
+
+	for i, fd := range blocks.FieldDescriptions() {
+		if fd.Name == "hash" {
+			hashPos = i
+		}
+	}
+
+	for i := 0; blocks.Next(); i++ {
+		values, err := blocks.Values()
+		if err != nil {
+			return err
+		}
+
+		for i, value := range values {
+			val, err := entities[EntBlock].transformers[i](value)
+			if err != nil {
+				return err
+			}
+
+			s := scalar.NewScalar(entities[EntBlock].arrowSchema.Field(i).Type)
+			if err := s.Set(val); err != nil {
+				return err
+			}
+
+			scalar.AppendToBuilder(entities[EntBlock].recBuilder.Field(i), s)
+		}
+
+		// TODO "misses" table
+
+		// transaction
+		c.query(ctx, entities[EntTransaction], sql2, append(args2, hashPos)...)
+		res <- &message.SyncInsert{Record: entities[EntTransaction].recBuilder.NewRecord()}
+
+		// NewRecord resets the builder for reuse
+		res <- &message.SyncInsert{Record: entities[EntBlock].recBuilder.NewRecord()}
+
+		if c.pluginSpec.Limit > 0 && (i+1) == c.pluginSpec.Limit {
+			return nil // exit
+		}
+	}
+
 	return nil
 }
 
